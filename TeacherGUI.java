781d8c82ddb946107fb314cd2a5bf08e941d4080
Will Zou <89487467+willzou29@users.noreply.github.com>
2021-12-06 14:04:42 -0500

    Update TeacherGUI.java

--- TeacherGUI.java
+++ TeacherGUI.java
@@ -21,16 +21,17 @@ public class TeacherGUI extends JComponent implements Runnable {
         JFrame frame = new JFrame("Teacher Menu");
         Container content = frame.getContentPane();
 
-        JButton optionsMenu = new JButton("Create Course");
-        optionsMenu.addActionListener(new ActionListener() {
+        JButton option1 = new JButton("Create Course");
+        option1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
                 createCourse();
             }
         });
-        JButton edit_course = new JButton("Edit Course");
-        edit_course.addActionListener(new ActionListener() {
+
+        JButton option2 = new JButton("Edit Course");
+        option2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
@@ -38,8 +39,8 @@ public class TeacherGUI extends JComponent implements Runnable {
             }
         });
 
-        JButton view_submissions = new JButton("View Submissions");
-        view_submissions.addActionListener(new ActionListener() {
+        JButton option3 = new JButton("View Submissions");
+        option3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
@@ -47,8 +48,8 @@ public class TeacherGUI extends JComponent implements Runnable {
             }
         });
 
-        JButton edit_information = new JButton("Edit Information");
-        edit_information.addActionListener(new ActionListener() {
+        JButton option4 = new JButton("Edit Information");
+        option3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
@@ -56,25 +57,26 @@ public class TeacherGUI extends JComponent implements Runnable {
             }
         });
 
-        JButton logout = new JButton("Logout");
-        logout.addActionListener(new ActionListener() {
+        JButton option5 = new JButton("Logout");
+        option3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
-                logout();
+                Logout();
             }
         });
+
 
         createUser = new JButton("Create User");
 //        createUser.addActionListener(actionListener);
 
         JPanel panel = new JPanel();
-        panel.add(optionsMenu);
-        panel.add(edit_course);
-        panel.add(view_submissions);
-        panel.add(edit_information);
-        panel.add(logout);
-        content.add(panel, BorderLayout.NORTH);
+        panel.add(option1);
+        panel.add(option2);
+        panel.add(option3);
+        panel.add(option4);
+        panel.add(option5);
+        content.add(panel, BorderLayout.CENTER);
 
         frame.setSize(400, 300);
         frame.setLocationRelativeTo(null);
@@ -83,20 +85,42 @@ public class TeacherGUI extends JComponent implements Runnable {
 
     }
     void createCourse() {
-        JFrame frame = new JFrame("Login");
+        JFrame frame = new JFrame("Create Course");
         Container content = frame.getContentPane();
-        JLabel usernameLabel = new JLabel("Username:");
-        JTextField username = new JTextField(10);
-        JLabel passwordLabel = new JLabel("Password");
-        JTextField password = new JTextField(10);
+        JLabel question = new JLabel("What do you want to name the course?");
+        JLabel question2 = new JLabel("Two letters and 3 numbers Ex. CS180");
+        JTextField courseName = new JTextField(10);
         JButton enter = new JButton("Enter");
 
 //        username.addActionListener(actionListener);
 //        password.addActionListener(actionListener);
 
         JPanel panel = new JPanel();
-        panel.add(usernameLabel);
-        panel.add(username);
+        panel.add(question);
+        panel.add(question2);
+        panel.add(courseName);
+
+        panel.add(enter);
+        content.add(panel, BorderLayout.CENTER);
+
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    void editCourse() {
+        JFrame frame = new JFrame("Edit Course");
+        Container content = frame.getContentPane();
+        JLabel question = new JLabel("Which course would you like to edit?");
+        JTextField courseEdit = new JTextField(10);
+        JLabel passwordLabel = new JLabel("Password");
+        JTextField password = new JTextField(10);
+        JButton enter = new JButton("Enter");
+
+        JPanel panel = new JPanel();
+        panel.add(question);
+        panel.add(courseEdit);
         panel.add(passwordLabel);
         panel.add(password);
         panel.add(enter);
@@ -106,52 +130,77 @@ public class TeacherGUI extends JComponent implements Runnable {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
-
-
-
-    }
-
-    void editCourse() {
-        JFrame frame = new JFrame("Edit Course");
-        Container content = frame.getContentPane();
-        JLabel usernameLabel = new JLabel("Username:");
-        JTextField username = new JTextField(10);
-        JLabel passwordLabel = new JLabel("Password");
-        JTextField password = new JTextField(10);
-        JButton enter = new JButton("Enter");
-
     }
 
     void viewSubmissions() {
         JFrame frame = new JFrame("View Submissions");
         Container content = frame.getContentPane();
-        JLabel usernameLabel = new JLabel("Username:");
-        JTextField username = new JTextField(10);
+        JLabel question = new JLabel("Which course would you like to edit?");
+        JTextField courseEdit = new JTextField(10);
         JLabel passwordLabel = new JLabel("Password");
         JTextField password = new JTextField(10);
         JButton enter = new JButton("Enter");
 
+        JPanel panel = new JPanel();
+        panel.add(question);
+        panel.add(courseEdit);
+        panel.add(passwordLabel);
+        panel.add(password);
+        panel.add(enter);
+        content.add(panel, BorderLayout.CENTER);
+
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     void editInformation() {
         JFrame frame = new JFrame("Edit Information");
         Container content = frame.getContentPane();
-        JLabel usernameLabel = new JLabel("Username:");
-        JTextField username = new JTextField(10);
+        JLabel question = new JLabel("Which course would you like to edit?");
+        JTextField courseEdit = new JTextField(10);
         JLabel passwordLabel = new JLabel("Password");
         JTextField password = new JTextField(10);
         JButton enter = new JButton("Enter");
 
+        JPanel panel = new JPanel();
+        panel.add(question);
+        panel.add(courseEdit);
+        panel.add(passwordLabel);
+        panel.add(password);
+        panel.add(enter);
+        content.add(panel, BorderLayout.CENTER);
+
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
     }
 
-    void logout() {
+    void Logout() {
         JFrame frame = new JFrame("Logout");
         Container content = frame.getContentPane();
-        JLabel usernameLabel = new JLabel("Username:");
-        JTextField username = new JTextField(10);
+        JLabel question = new JLabel("Which course would you like to edit?");
+        JTextField courseEdit = new JTextField(10);
         JLabel passwordLabel = new JLabel("Password");
         JTextField password = new JTextField(10);
         JButton enter = new JButton("Enter");
 
+        JPanel panel = new JPanel();
+        panel.add(question);
+        panel.add(courseEdit);
+        panel.add(passwordLabel);
+        panel.add(password);
+        panel.add(enter);
+        content.add(panel, BorderLayout.CENTER);
+
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
     }
+
+
+
 }
