c12e10405eca8b5e6b80cb0f0a38ef301d430580
Chinmay Bansal <41792808+ChinmayBansal@users.noreply.github.com>
2021-12-06 14:26:54 -0500

    Converted Guis class into client and was able to send message to server

--- ClientTest.java
+++ ClientTest.java
@@ -1,59 +1,127 @@
-// Java implementation for a client
-// Save file as Client.java
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 // Client class
-public class ClientTest
+public class ClientTest extends JComponent implements Runnable
 {
+    private static Socket s;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+
     public static void main(String[] args) throws IOException
     {
         try
         {
-            Scanner scn = new Scanner(System.in);
-
             // getting localhost ip
             InetAddress ip = InetAddress.getByName("localhost");
 
             // establish the connection with server port 5056
-            Socket s = new Socket(ip, 5056);
+            s = new Socket(ip, 4242);
 
-            // obtaining input and out streams
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            SwingUtilities.invokeLater(new ClientTest());
 
-            // the following loop performs the exchange of
-            // information between client and client handler
-            while (true)
-            {
-                System.out.println(dis.readUTF());
-                String tosend = scn.nextLine();
-                dos.writeUTF(tosend);
-
-                // If client sends exit,close this connection
-                // and then break from the while loop
-                if(tosend.equals("Exit"))
-                {
-                    System.out.println("Closing this connection : " + s);
-                    s.close();
-                    System.out.println("Connection closed");
-                    break;
-                }
-
-                // printing date or time as requested by client
-                String received = dis.readUTF();
-                System.out.println(received);
-            }
-
-            // closing resources
-            scn.close();
-            dis.close();
-            dos.close();
+//           dis.close();
+//           dos.close();
         }catch(Exception e){
             e.printStackTrace();
         }
     }
+
+    public static synchronized String messageToServer(String message) throws IOException {
+        // obtaining input and out streams
+        while (true) {
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeUTF(message);
+
+        }
+    }
+
+    @Override
+    public void run() {
+
+        // First Gui
+        JFrame frame = new JFrame("Quiz App");
+        Container content = frame.getContentPane();
+        JButton login = new JButton("Login");
+        JButton createUser = new JButton("Create User");
+        JPanel panel = new JPanel();
+        panel.add(login);
+        panel.add(createUser);
+        content.add(panel, BorderLayout.NORTH);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        createUser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+
+                JFrame frame = new JFrame("Create User");
+                Container content = frame.getContentPane();
+                JButton teacherButton = new JButton("Teacher");
+                JButton studentButton = new JButton("Student");
+                JPanel panel = new JPanel();
+                panel.add(teacherButton);
+                panel.add(studentButton);
+                content.add(panel, BorderLayout.CENTER);
+                frame.setSize(600, 400);
+                frame.setLocationRelativeTo(null);
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame.setVisible(true);
+
+                teacherButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        frame.dispose();
+
+                        JFrame frame = new JFrame("Create User");
+                        Container content = frame.getContentPane();
+                        JLabel teacherUsername = new JLabel("Enter Username");
+                        JTextField enterTeacherUsername = new JTextField(10);
+                        JLabel teacherPassword = new JLabel("Enter Password");
+                        JTextField enterTeacherPassword = new JTextField(10);
+                        JButton enter = new JButton("Enter");
+                        JPanel panel = new JPanel();
+                        panel.add(teacherUsername);
+                        panel.add(enterTeacherUsername);
+                        panel.add(teacherPassword);
+                        panel.add(enterTeacherPassword);
+                        panel.add(enter);
+                        content.add(panel, BorderLayout.CENTER);
+                        frame.setSize(600, 400);
+                        frame.setLocationRelativeTo(null);
+                        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        frame.setVisible(true);
+                        enter.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                try {
+                                    messageToServer(enterTeacherUsername.getText());
+                                } catch (IOException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+    }
+
 }
 
